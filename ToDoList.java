8848f61dcef92317e4a329fb0bbf42d0a02bc557
PedroBarge <pedro525tavares@gmail.com>
2023-10-27 12:08:34 +0100

    v1 done

--- ToDoList.java
+++ ToDoList.java
@@ -29,8 +29,8 @@ public class ToDoList {
             System.out.println("\n\u001b[38;5;15mYou still have \u001b[38;5;11m" + count + "\u001b[38;5;15m free spaces on the list!\u001b[0m");
 
 //            System.out.println("\n\u001b[38;5;15m " + Arrays.toString(toDoList) + "\u001b[0m");
-
-            System.out.println("\n\u001b[38;5;15m1 - Show ToDoList\u001b[0m");
+            System.out.println();
+            System.out.println("\u001b[38;5;15m1 - Show ToDoList\u001b[0m");
             System.out.println("\u001b[38;5;15m2 - Create task\u001b[0m");
             System.out.println("\u001b[38;5;15m3 - Mark as completed\u001b[0m");
             System.out.println("\u001b[38;5;15m4 - Remove as completed\u001b[0m");
